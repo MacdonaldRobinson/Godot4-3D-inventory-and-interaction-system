0863938bc0527c575ba24d024854f98a58c6060e
MacdonaldRobinson <maocnly2001@gmail.com>
2023-04-02 10:50:18 -0600

    Restructured files and updated the Player Controller to make it easier to attach to  Player

---
+++ Player/Player.gd
@@ -0,0 +1,4 @@
+extends Node3D
+class_name Player
+
+@onready var player_controller: PlayerController = $PlayerController
--- PlayerController/PlayerController.gd
+++ PlayerController/PlayerController.gd
@@ -1,4 +1,4 @@
-extends CharacterBody3D
+extends Node3D
 class_name PlayerController
 
 const SPEED = 5.0
@@ -7,32 +7,34 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_controller:CameraController = $CameraController
-@onready var eyes_cast: RayCast3D = $EyesCast
 @onready  var inventory_controller: InventoryController = $InventoryController
 
+@export var eyes_cast: RayCast3D 
+@export var player: Player
+
 func _ready():
-	velocity = Vector3.ZERO
+	player.velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if not player.is_on_floor():
+		player.velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and player.is_on_floor():
+		player.velocity.y = JUMP_VELOCITY
 	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	var camera_look_at_point:Vector3 = camera_controller.get_camera_look_at_point()		
 	eyes_cast.look_at(camera_look_at_point)	
 
 	if direction:		
-		self.look_at(Vector3(camera_look_at_point.x, 0, camera_look_at_point.z))
+		player.look_at(Vector3(camera_look_at_point.x, 0, camera_look_at_point.z))
 		
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		player.velocity.x = direction.x * SPEED
+		player.velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		player.velocity.z = move_toward(player.velocity.z, 0, SPEED)
 		
-	move_and_slide()
+	player.move_and_slide()
